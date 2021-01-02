437fb0da41c129d40f7c467cef37a86e3237e3e9
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-02 16:26:14 +0100

    added gitignore

--- Summing2/src/main/java/Practice/Main.java
+++ Summing2/src/main/java/Practice/Main.java
@@ -2,16 +2,17 @@ package Practice;
 
 public class Main {
     public static void main(String[] args) {
-        //Please correct the code below so that the result is displayed on the screen.
-        // int x = 10;
-        // int y = 4;
-        // int div = x / y;
-        // System.out.println(div);
-        // Expected result is 2.5
+        /** Please correct the code below so that the result is displayed on the screen.
+         int x = 10;
+         int y = 4;
+         int div = x / y;
+         System.out.println(div);
+         Expected result is 2.5
+        **/
 
         /**
          * I can either change the type of variables from int to double.
-         * Second way is to projecting the div from int to double
+         * Second way is projecting the div from int to double
          */
         int x = 10;
         int y = 4;
