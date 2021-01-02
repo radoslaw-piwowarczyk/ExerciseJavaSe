445c77effd6e04f894b79352b9a992b2eb32dac4
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-02 16:34:27 +0100

    write using unicode

---
+++ KotEx/src/main/java/Practice/KotPrinter.java
@@ -0,0 +1,21 @@
+package Practice;
+
+public class KotPrinter {
+
+    /** Write a program that will display the word CAT on the screen.
+    You cannot use a single quotation mark or single single
+    quotation mark in the program
+     **/
+    public static void main(String[] args) {
+        char k = 0x004B;
+        char o = 0x004F;
+        char t = 0x0054;
+        System.out.println(k);
+        System.out.println(o);
+        System.out.println(t);
+    }
+
+
+
+
+}
