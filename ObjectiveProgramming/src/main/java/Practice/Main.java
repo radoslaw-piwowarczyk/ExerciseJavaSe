0161316586471e868371ca0aa6ed81048df499c9
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-03 19:28:10 +0100

    main class refactor

---
+++ ObjectiveProgramming/src/main/java/Practice/Main.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 5;
+        increaseValueBy10(number);
+        System.out.println(number);
+    }
+    public static void increaseValueBy10 ( int x){
+        x = x + 10;
+    }
+}
