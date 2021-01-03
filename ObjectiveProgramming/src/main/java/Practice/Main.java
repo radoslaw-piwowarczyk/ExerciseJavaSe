77c9bc7439b7ea505790f4ef08a5aa33707c72a8
Radoslaw Piwowarczyk <rad.piwowarczyk@gmail.com>
2021-01-03 19:36:58 +0100

    created Employee, PaymentCalculator, Main classes

--- ObjectiveProgramming/src/main/java/Practice/Main.java
+++ ObjectiveProgramming/src/main/java/Practice/Main.java
@@ -1,12 +1,2 @@
-package Practice;
-
-public class Main {
-    public static void main(String[] args) {
-        int number = 5;
-        increaseValueBy10(number);
-        System.out.println(number);
-    }
-    public static void increaseValueBy10 ( int x){
-        x = x + 10;
-    }
+package Practice;public class Main {
 }
